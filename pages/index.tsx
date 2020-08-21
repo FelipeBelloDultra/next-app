cb6c754e81ae19d34c521e7970faa0455d9daa46
FelipeBelloDultra <felipe_bello_dultra@hotmail.com>
2020-08-21 20:15:00 -0300

    fix: Global styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,18 @@
+import Head from 'next/head';
+
+import GlobalStyles from '../styles/GlobalStyles';
+
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+    <>
+      <Head>
+        <title>Felipe Bello Dultra</title>
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500;700&display=swap" rel="stylesheet" />
+      </Head>
+      <main>
+        <h1>Olá</h1>
+        <GlobalStyles />
+      </main>
+    </>
   )
 }
